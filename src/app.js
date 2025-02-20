20b4d4ca6a8bf57d2744930cb6c8e2a51b964a0a
iabbas21 <i.irfanabbas1996@gmail.com>
2025-02-20 14:31:43 +0530

    GET api's

--- src/app.js
+++ src/app.js
@@ -16,6 +16,38 @@ app.post('/signup', async (req, res) => {
     }
 })
 
+// GET /user - get a user from DB
+app.get('/user', async (req, res) => {
+    try {
+        const user = await User.findOne({ emailId: req.body.emailId })
+        if(user) {
+            res.send(user)
+        } else {
+            res.status(404).send("User not found...")
+        }
+        // const users = await User.find({ emailId: req.body.emailId })
+        // if(users.length > 0) {
+        //     res.send(users)
+        // } else {
+        //     res.status(404).send("User not found...")
+        // }
+    } catch(error) {
+        console.log(error)
+        res.status(500).send("Error fetching user...")
+    }
+})
+
+// GET /feed - get all the users from DB
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch(error) {
+        console.log(error)
+        res.status(500).send("Error fetching users...")
+    }
+})
+
 const PORT = 3000;
 
 // First connect to the database then start the server
