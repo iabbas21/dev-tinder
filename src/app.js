45dfc0e72198866afa85657e1705e017781f662e
iabbas21 <i.irfanabbas1996@gmail.com>
2025-02-22 12:47:36 +0530

    created jwt token, added token in cookie and created auth middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
-const validator = require('validator')
 const connectDB = require('./config/database')
 const app = express()
 const User = require('./models/user')
 const { validateSignUpData } = require('./utils/validation')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const { userAuth } = require('./middlewares/auth')
 
 // Middleware to parse the incoming request body
 app.use(express.json())
+app.use(cookieParser())
 
 app.post('/signup', async (req, res) => {
     try {
@@ -53,82 +57,39 @@ app.post('/login', async (req, res) => {
 
         const isPasswordMatched = await bcrypt.compare(password, user.password)
         if (isPasswordMatched) {
+            // Create JWT Token
+            const token = await jwt.sign({ _id: user._id }, 'Dev@Tinder$2025', { expiresIn: '7d' })
+            
+            // Add the token to the cookie and send the response to the user
+            res.cookie("token", token, { expires: new Date(Date.now() + 7 * 86400000), httpOnly: true })
             res.send('Login successful...')
         } else {
             throw new Error('Invalid credentials...')
         }
-
-        res.send(user)
     } catch (error) {
         res.status(400).send(error.message)
     }
 })
 
-// GET /user - get a user from DB
-app.get('/user', async (req, res) => {
+// GET /profile
+app.get('/profile', userAuth, async (req, res) => {
     try {
-        const user = await User.findOne({ emailId: req.body.emailId })
-        if(user) {
-            res.send(user)
-        } else {
-            res.status(404).send("User not found...")
-        }
-        // const users = await User.find({ emailId: req.body.emailId })
-        // if(users.length > 0) {
-        //     res.send(users)
-        // } else {
-        //     res.status(404).send("User not found...")
-        // }
+        const user = req.user
+        res.send(user)
     } catch(error) {
-        console.log(error)
-        res.status(500).send("Error fetching user...")
+        res.status(400).send(error.message)
     }
 })
 
-// GET /feed - get all the users from DB
-app.get('/feed', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(error) {
-        console.log(error)
-        res.status(500).send("Error fetching users...")
-    }
+// POST /sendConnectionRequest
+app.post('/sendConnectionRequest', userAuth, (req, res) => {
+    const user = req.user
+    // Send connection request
+    console.log('Send connection request')
+
+    res.send(user.firstName + 'sent connection request')
 })
 
-// DELETE /user - Delete a user from DB
-app.delete('/user', async (req, res) => {
-    const userId = req.body.userId
-    try {
-        // await User.findByIdAndDelete(userId)
-        await User.findOneAndDelete({ _id: userId })
-        res.send("User deleted successfully...")
-    } catch(error) {
-        res.status(500).send("Error deleting user...")
-    }
-})
-
-// PATCH /user - Update a user document in DB
-app.patch('/user/:userId', async (req, res) => {
-    const userId = req.params.userId
-    const dataToUpdate = req.body
-    try {
-        const ALLOWED_UPDATES = ['gender', 'age', 'photoUrl', 'about', 'skills']
-        const isAllowed = Object.keys(dataToUpdate).every(update => ALLOWED_UPDATES.includes(update))
-        if(!isAllowed) {
-            return res.status(400).send("Update not allowed...")
-        }
-        if(dataToUpdate?.skills && dataToUpdate.skills.length > 10) {
-            return res.status(400).send("Skills limit exceeded...")
-        }
-        await User.findByIdAndUpdate(userId, dataToUpdate, {
-            runValidators: true,
-        })
-        res.send("User updated successfully...")
-    } catch(error) {
-        res.status(500).send("Error updating user...")
-    }
-})
 
 const PORT = 3000;
 
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,27 +1,29 @@
-const adminAuth = (req, res, next) => {
-    console.log('Authorize Admin!')
-    // Logic to check if user is admin
-    const token = 'abc123'
-    const isAdminAuthorized = token === 'abc123'
-    if (!isAdminAuthorized) {
-        res.status(401).send('Unauthorized')
-    } else {
-        next()
-    }
-}
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
-const userAuth = (req, res, next) => {
-    console.log('Authorize User!')
-    const token = 'xyz123'
-    const isUserAuthorized = token === 'xyz123'
-    if(!isUserAuthorized) {
-        res.status(401).send('Unauthorized')
-    } else {
+const userAuth = async (req, res, next) => {
+    try {
+        // Extract token from cookies
+        const { token } = req.cookies
+        if(!token) {
+            throw new Error('Token is invalid')
+        }
+
+        // Validate token by extracting hidden secret
+        const decoded = jwt.verify(token, 'Dev@Tinder$2025')
+
+        // Fetch user with _id from DB
+        const user = await User.findById(decoded._id)
+        if(!user) {
+            throw new Error('User not found')
+        }
+        req.user = user
         next()
+    } catch(error) {
+        res.status(400).send(error.message)
     }
 }
 
 module.exports = {
-    adminAuth,
     userAuth
 }
